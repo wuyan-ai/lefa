bfa6156dcf5bd96fa0d0c84b8fb103cc87772c4d
zxy <3102194154@qq.com>
2021-07-31 11:45:00 +0800

    初始化后台

---
+++ NovaIDEA/src/main/java/com/nova/novaidea/NovaIdeaApplication.java
@@ -0,0 +1,13 @@
+package com.nova.novaidea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaIdeaApplication.class, args);
+    }
+
+}
---
+++ NovaIDEA/src/test/java/com/nova/novaidea/NovaIdeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nova.novaidea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
